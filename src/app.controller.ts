48cc369609ce8a3db2d0fbe824d93bb51873adaf
Leonardo Treviso <leonardosistemas@outlook.com>
2021-08-03 23:13:26 -0300

    Criando controller para receber requisição e enviar para o rabbit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Logger, Post } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1/categorias')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private logger = new Logger(AppController.name)
+
+  private clientAdminBackend : ClientProxy
+
+  constructor() {
+    console.log('construtor')
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options:{
+        urls: ['amqp://user:bitnami@localhost:5672/smartranking'],
+        queue:'admin-backend',
+        queueOptions: {
+          durable: false
+        },
+      }
+    })
+
   }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto : CriarCategoriaDto){
+
+
+    /* return await this.clientAdminBackend.emit('criar-Categoria',criarCategoriaDto) */
+
+    console.log('Chegou aqui')
+    this.clientAdminBackend.emit('criar-categoria',criarCategoriaDto)
+  }
+  
 }
